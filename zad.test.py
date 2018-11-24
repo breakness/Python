6a8fd12a215fcbe7e52bc48a3d940afab99c129b
breakness <freshu@gmail.com>
2018-11-24 23:56:05 +0100

    ver 0.1

--- zad.test.py
+++ zad.test.py
@@ -1,3 +1,5 @@
-#test
-
-#zmiana1
+import string
+from random import *
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(8, 16)))
+password
